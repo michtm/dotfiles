800a4c7bb952ac4b7965a400b597beb5b506a663
michtm <michel.tring@gmail.com>
2023-10-02 00:53:31 +0200

    Add Emacs and update install.sh

---
+++ emacs/early-init.el
@@ -0,0 +1,34 @@
+;;; early-init.el --- Early Init File -*- lexical-binding: t -*-
+
+;;; Commentary:
+
+;; Les commentaires sont écrits en français et en anglais, dans l'ordre de
+;; priorité
+
+;;; Code:
+
+;; Augmente le seuil du ramasse-miettes de façon temporaire
+(defvar early-init--gc-cons-threshold gc-cons-threshold)
+
+(setq gc-cons-threshold most-positive-fixnum)
+
+;; NOTE(mtr) Tout ce qui concerne l'interface utilisateur fera l'objet d'un
+;; thème Emacs (nom de code: Vision)
+
+;; Activation de quelques fonctionnalités d'interface utilisateur
+(setq inhibit-startup-screen t ; N'affiche pas l'écran de démarrage
+      visible-bell t)          ; Faire clignoter le cadre pour la cloche
+
+(column-number-mode)
+(global-display-line-numbers-mode 1)
+
+;; Désactivation de certains éléments d'interface utilisateur
+(menu-bar-mode -1)
+(scroll-bar-mode -1)
+(tool-bar-mode -1)
+
+(add-hook 'emacs-startup-hook
+	  (lambda ()
+	    (setq gc-cons-threshold early-init--gc-cons-threshold)))
+
+;;; early-init.el ends here
--- install.sh
+++ install.sh
@@ -5,6 +5,10 @@
 script_path="$(readlink -f "$0")"
 script_dir="$(dirname "$script_path")"
 
+# Installation des fichiers de démarrage de Emacs
+mkdir -p ~/.emacs.d
+ln -fs "$script_dir/emacs/early-init.el" ~/.emacs.d/early-init.el
+
 # Suppression des mots
 unset -v script_dir
 unset -v script_path
