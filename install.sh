7a943de6e8e007c123427ac0715e79fed14a6d61
michtm <michel.tring@gmail.com>
2023-10-01 07:07:12 +0200

    (fr) Suppression de Neovim (pour le moment...)

--- install.sh
+++ install.sh
@@ -5,9 +5,6 @@
 script_path="$(readlink -f "$0")"
 script_dir="$(dirname "$script_path")"
 
-# Neovim
-ln -fs "$script_dir/nvim/init.lua" ~/.config/nvim/init.lua
-
 # Suppression des mots
 unset -v script_dir
 unset -v script_path
--- nvim/init.lua
+++
@@ -1,8 +0,0 @@
--- init.lua: Là où tout commence
-
--- Globals
-vim.g.loaded_python_provider = 0
-vim.g.loaded_perl_provider   = 0
-
--- Options
-vim.opt.number = true
